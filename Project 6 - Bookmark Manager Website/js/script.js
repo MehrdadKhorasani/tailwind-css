f92bd3959fc74f8561b5dfd2e5e3192adf867f58
MehrdadKhorasani <mehrdadaria9@gmail.com>
2024-03-04 15:35:55 +0330

    Project 6 - done

---
+++ Project 6 - Bookmark Manager Website/js/script.js
@@ -0,0 +1,42 @@
+const tabs = document.querySelectorAll(".tab");
+const panels = document.querySelectorAll(".panel");
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const logo = document.getElementById("logo");
+
+tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
+btn.addEventListener("click", navToggle);
+
+function onTabClick(e) {
+  // DEACTIVE ALL TABS
+  tabs.forEach((tab) => {
+    tab.children[0].classList.remove(
+      "border-softRed",
+      "border-b-4",
+      "md:border-b-0"
+    );
+  });
+
+  // HIDE ALL PANELS
+  panels.forEach((panel) => panel.classList.add("hidden"));
+
+  // ACTIVE A NEW TAB AND PANEL
+  e.target.classList.add("border-softRed", "border-b-4");
+  const classString = e.target.getAttribute("data-target");
+  document
+    .getElementById("panels")
+    .getElementsByClassName(classString)[0]
+    .classList.remove("hidden");
+}
+
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+
+  if (menu.classList.contains("flex")) {
+    logo.setAttribute("src", "./images/logo-bookmark-footer.svg");
+  } else {
+    logo.setAttribute("src", "./images/logo-bookmark.svg");
+  }
+}
